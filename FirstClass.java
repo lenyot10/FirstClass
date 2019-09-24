97d27f5ba4171093c58f5577f9e2a91d4091bbe5
Aljabar Azis <aljabarazis14@gmail.com>
2019-09-24 16:19:15 +0700

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,63 @@
+public class FirstClass{
+    public static void main(String[]args){
+    /*    //Tipe data Pada Java
+       char nilai ; 
+        String nama ;
+        int umur;
+        int harga;
+        double ukuran;
+        float f =(float)2.5;
+        ukuran = 178.4;
+        harga = 100000;
+        umur = 18;
+        nilai = 'A';
+        nama = "ajit";
+
+
+        System.out.println("Hello Word");
+        System.out.println("Nilai"+nilai);
+        System.out.println("Siapakah Nama Kamu ? " +nama);
+        System.out.println("Berapakah Umur Kamu ? " +umur);
+        System.out.println("Berapakah Harga Baju ini ? Rp."+harga);
+        System.out.println("Berapakah Ukuran Badan Kamu ? "+ukuran);
+        System.out.println("f"+f);
+*/
+//Operator perbandingan
+      /*  Integer angka1,angka2,i,a;
+        Integer hasil1,hasil2,hasil3;
+        angka1 = 25;
+        angka2 = 25;
+        i = 20;
+        a =i+5;
+        hasil1 = angka1 * angka2;
+        hasil2 = angka1 + angka2;
+        hasil3 = hasil1 / angka2;
+        System.out.println("Hasil Perkalian dari "+ angka1+ " * "+ angka2 + " = "+hasil1);
+        System.out.println("Hasil Perkalian dari "+ angka1+ " + "+ angka2 + " = "+hasil2);
+        System.out.println("Hasil Perkalian dari "+ hasil1+ " : "+ angka2 + " = "+hasil3);
+        System.out.println(a);
+
+      */
+     /* int a = 21;
+    */
+
+    //Operator logika
+        Boolean a = true;
+        Boolean b = false;
+
+        System.out.println("a && b = "+(a & b));
+        System.out.println("a && a = "+(a & a));
+        System.out.println("b && b = "+(b & b));
+        //OR 
+        System.out.println("a || b = "+(a || b));
+        System.out.println("b || b = "+(b || b));
+        System.out.println("a || a = "+(a || a));
+        System.out.println("!(a && b) = "+!(a && b));
+        System.out.println("!(a) = "+!(a));
+        System.out.println("!(b) = "+!(b)); 
+    
+
+    
+    }
+}
+
